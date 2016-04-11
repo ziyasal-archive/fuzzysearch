fc1ad5a62ec96f599617f3a593646be630db242d
Ziya SARIKAYA <sarikayaziya@gmail.com>
2016-04-12 01:02:22 +0300

    Update fuzzysearch_test.go

--- fuzzysearch_test.go
+++ fuzzysearch_test.go
@@ -1,1 +1,12 @@
 
+package main
+
+import "testing"
+
+func TestFuzzySearch(t *testing.T) {
+  var v bool
+  v = FuzzySearch("twl", "cartwheel")
+  if v != true {
+    t.Error("Expected true, got ", v)
+  }
+}
